5fe9e90a226e812d58f5436ac38d3e551ee71c89
Myrmex <dfusi@hotmail.com>
2023-01-20 14:42:24 +0100

    Updated packages

--- src/app/index-lookup-definitions.ts
+++ src/app/index-lookup-definitions.ts
@@ -5,6 +5,6 @@ export const INDEX_LOOKUP_DEFINITIONS: IndexLookupDefinitions = {
   // human-friendly ID for sites
   site: {
     typeId: METADATA_PART_TYPEID,
-    name: 'hid',
+    name: 'eid',
   },
 };
